471b51d7961a80806e00bd176e341aff1cc99fc7
Matthew Voss <voss.matthew@gmail.com>
2018-04-23 15:33:12 -0500

    update trans download

--- scripts/downloadTransactions.js
+++ scripts/downloadTransactions.js
@@ -39,7 +39,6 @@ const db = knex({
     database : config.mysql.database,
     port: config.mysql.port || 3306,
   },
-  debug: ['ComQueryPacket'],
 });
 const finish = () => {
   process.exit();
@@ -75,6 +74,13 @@ const getBatchIds = (batch) => {
         if (batchItems.length) {
           retVal = batchItems[0].getBatchId();
         }
+      } else {
+        if (response.getTransactionResponse() != null && response.getTransactionResponse().getErrors() != null) {
+          retval = response.getTransactionResponse().getErrors().getError();
+				}
+				else {
+          retVal = response.getMessages().getMessage();
+				}
       }
 
       resolve(retVal);
@@ -83,11 +89,11 @@ const getBatchIds = (batch) => {
 }
 
 const getTransaction = (trans) => {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     let retVal;
     const getRequest = new ApiContracts.GetTransactionDetailsRequest();
 	  getRequest.setMerchantAuthentication(merchantAuthenticationType);
-	  getRequest.setTransId(trans.transId);
+	  getRequest.setTransId(trans.transactionId);
 	  const ctrl = new ApiControllers.GetTransactionDetailsController(getRequest.getJSON());
     ctrl.setEnvironment(SDKConstants.endpoint.production);
     ctrl.execute(() => {
@@ -98,6 +104,8 @@ const getTransaction = (trans) => {
         && response.getMessages().getResultCode() == ApiContracts.MessageTypeEnum.OK
       ) {
         retVal = response.getTransaction();
+      } else {
+        reject(response.getMessages().getMessage());
       }
       resolve(retVal);
     });
@@ -136,76 +144,91 @@ const getTransactions = (batchId) => {
 
 const updateTransaction = async (transaction) => {
   let results;
-  const record = {
-    transId: transaction.transId,
-    submitTimeUTC: moment.tz(transaction.submitTimeUTC, 'UTC').format('YYYY-MM-DD HH:mm:ss'),
-    submitTimeLocal: moment(transaction.submitTimeLocal).format('YYYY-MM-DD HH:mm:ss'),
-    transactionType: transaction.transactionType,
-    transactionStatus: transaction.transactionStatus,
-    responseCode: transaction.responseCode,
-    responseReasonCode: transaction.responseReasonCode,
-    responseReasonDescription: transaction.responseReasonDescription,
-    authCode: transaction.authCode,
-    AVSResponse: transaction.AVSResponse,
-    cardCodeResponse: transaction.cardCodeResponse,
-    batchId: transaction.batch.batchId,
-    settlementTimeUTC: moment.tz(transaction.batch.settlementTimeUTC, 'UTC').format('YYYY-MM-DD HH:mm:ss'),
-    settlementTimeLocal: moment(transaction.batch.settlementTimeLocal).format('YYYY-MM-DD HH:mm:ss'),
-    invoiceNumber: transaction.order.invoiceNumber,
-    customerId: ('id' in transaction.customer) ? transaction.customer.id : null,
-    authAmount: transaction.authAmount,
-    settleAmount: transaction.settleAmount,
-    cardNumber: (transaction.payment && transaction.payment.creditCard) ? transaction.payment.creditCard.cardNumber : null,
-    cardType: (transaction.payment && transaction.payment.creditCard) ? transaction.payment.creditCard.cardType : null,
-    email: transaction.customer.email,
-  };
+  if (transaction) {
+    const record = {
+      transId: transaction.transId,
+      submitTimeUTC: moment.tz(transaction.submitTimeUTC, 'UTC').format('YYYY-MM-DD HH:mm:ss'),
+      submitTimeLocal: moment(transaction.submitTimeLocal).format('YYYY-MM-DD HH:mm:ss'),
+      transactionType: transaction.transactionType,
+      transactionStatus: transaction.transactionStatus,
+      description: transaction.order.description,
+      responseCode: transaction.responseCode,
+      responseReasonCode: transaction.responseReasonCode,
+      responseReasonDescription: transaction.responseReasonDescription,
+      authCode: transaction.authCode,
+      AVSResponse: transaction.AVSResponse,
+      cardCodeResponse: transaction.cardCodeResponse,
+      batchId: transaction.batch.batchId,
+      settlementTimeUTC: moment.tz(transaction.batch.settlementTimeUTC, 'UTC').format('YYYY-MM-DD HH:mm:ss'),
+      settlementTimeLocal: moment(transaction.batch.settlementTimeLocal).format('YYYY-MM-DD HH:mm:ss'),
+      invoiceNumber: transaction.order.invoiceNumber,
+      customerId: ('id' in transaction.customer) ? transaction.customer.id : null,
+      authAmount: transaction.authAmount,
+      settleAmount: transaction.settleAmount,
+      cardNumber: (transaction.payment && transaction.payment.creditCard) ? transaction.payment.creditCard.cardNumber : null,
+      cardType: (transaction.payment && transaction.payment.creditCard) ? transaction.payment.creditCard.cardType : null,
+      email: transaction.customer.email,
+    };
 
-  const existTransaction = await db('transactions')
-    .where({
-      transId: record.transId,
-    })
-    .catch(e => console.log('db', 'database error', e));
-  if (existTransaction.length) {
-    results = await db('transactions')
-      .where({ id: existTransaction[0].id })
-      .update(record)
-      .then(
-        data => db('transactions').where({ id: existTransaction[0].id }),
-      )
+    const existTransaction = await db('transactions')
+      .where({
+        transId: record.transId,
+      })
       .catch(e => console.log('db', 'database error', e));
+    if (existTransaction.length) {
+      results = await db('transactions')
+        .where({ id: existTransaction[0].id })
+        .update(record)
+        .then(
+          data => db('transactions').where({ id: existTransaction[0].id }),
+        )
+        .catch(e => console.error('db', 'database error', e));
+    } else {
+      results = await db('transactions')
+        .insert(record)
+        .then(
+          data => db('transactions').where({ id: data[0] }),
+        )
+        .catch(e => console.error('db', 'database error', e));
+    }
   } else {
-    results = await db('transactions')
-      .insert(record)
-      .then(
-        data => db('transactions').where({ id: data[0] }),
-      )
-      .catch(e => console.log('db', 'database error', e));
+    console.error('Missing Transaction:', transaction);
   }
-
   return results;
 }
 
 const getBatchDates = async () => {
+  /*
   const dates = await db('onsiteAttendees')
     .select('createdAt as batchDate')
     .distinct()
     .orderBy('batchDate', 'ASC')
     .catch(e => console.log('db', 'database error', e));
-  
-  if (dates.length) {
-    const batches = await map(dates, getBatchIds);
-    const filtered = [...new Set(batches)].filter(r => typeof r !== 'undefined');
+  */
+ const transactions = await db('registrantTransactions')
+  .whereNot(
+    {
+      type: 'check',
+      transactionId: '',
+    }
+  )
+  .catch(e => console.log('db', 'database error', e));
+  if (transactions.length) {
+    // const batches = await map(dates, getBatchIds);
+    // const filtered = [...new Set(batches)].filter(r => typeof r !== 'undefined');
     // console.log(filtered);
-    let transactions = await map(filtered, getTransactions);
+    //let transactions = await map(filtered, getTransactions);
+    /**
     const filteredTrans = [];
     transactions.forEach(batch => {
       batch.forEach(t => {
         filteredTrans.push(t);
       })
     })
+    */
     // console.log(filteredTrans);
-    const details = await map(filteredTrans, getTransaction);
-    console.log(details);
+    const details = await map(transactions, getTransaction);
+    // console.log(details);
     const results = await map(details, updateTransaction);
   }
 
